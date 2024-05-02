4c4436643e5d3c5066f5edaab705fcb4ca42cd23
SysoevDmitro <113758312+SysoevDmitro@users.noreply.github.com>
2024-05-02 13:12:02 +0300

    Update views.py

--- cinema/views.py
+++ cinema/views.py
@@ -45,10 +45,9 @@ class GenreDetail(APIView):
 
     def patch(self, request, pk):
         serializer = GenreSerializer(data=request.data)
-        if serializer.is_valid():
+        raise_exception=True
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         genres = self.get_object(pk)
